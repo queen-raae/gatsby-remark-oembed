8009ed8a67cf925716f99ed7e4e5116869ee062e
Benedicte Raae <raae@bgraphic.no>
2018-07-25 16:06:34 +0200

    Conceptually it works, but there are issues.

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+const Promise = require(`bluebird`);
+
 const {
+  fetchOembedProviders,
+  fetchOembed,
+  getProviderEndpointUrlForLinkUrl,
   selectPossibleOembedLinks,
   tranformsLinkNodeToOembedNode
 } = require("./helpers");
 
+const nodePromise = (node, providers) => {
+  const endpointUrl = getProviderEndpointUrlForLinkUrl(node.url, providers);
+  return (
+    endpointUrl &&
+    fetchOembed(node.url, endpointUrl).then(response => {
+      return tranformsLinkNodeToOembedNode(node, response);
+    })
+  );
+};
+
 module.exports = ({ markdownAST }) => {
   // Step 2: Find link nodes in markdown structure that are on their own, not part of some other content.
-  let possibleOmbedUrlNodes = selectPossibleOembedLinks(markdownAST);
-  // For each node
-  possibleOmbedUrlNodes.map(node => {
-    // Step 3: Check if url matched any of the oembed url schemes.
-    // Step 4: Fetch the oembed response from the oembed provider.
-    // Step 5:  Transform the link node into an html node using html value from the oembed response.
-    tranformsLinkNodeToOembedNode(node, {
-      html: "HTML as told to us by the oembed response"
-    });
+  const possibleOmbedUrlNodes = selectPossibleOembedLinks(markdownAST);
+
+  return fetchOembedProviders().then(providers => {
+    return Promise.all(
+      possibleOmbedUrlNodes.map(node => nodePromise(node, providers))
+    );
   });
-  return markdownAST;
 };
