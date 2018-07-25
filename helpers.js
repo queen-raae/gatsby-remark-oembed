64261b07bcde72c8a7983edd2e8bdd415890c3c5
Benedicte Raae <raae@bgraphic.no>
2018-07-25 14:19:49 +0200

    Added function for fetching oembed with test.

--- helpers.js
+++ helpers.js
@@ -1,4 +1,12 @@
 const select = require("unist-util-select");
+const axios = require("axios");
+
+const OEMBED_PROVIDERS_URL = "https://oembed.com/providers.json";
+
+exports.fetchOembedProviders = async () => {
+  const response = await axios.get(OEMBED_PROVIDERS_URL);
+  return response.data;
+};
 
 exports.selectPossibleOembedLinks = markdownAST => {
   return select(markdownAST, "paragraph link:only-child");
