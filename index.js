fb617f8f3a1c04743654e46122b3ff061abf08e2
Benedicte Raae <raae@bgraphic.no>
2018-09-08 18:41:57 +0200

    Remove uneeded console log

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ const {
 } = require("./helpers");
 
 module.exports = async ({ markdownAST }) => {
-  // Let's see if this works
-  console.log(
-    "nå er jeg i plugins/Gatsby-remark-oembed index.js etter const endpointUrl"
-  );
   try {
     // Step 1.  Fetch the oembed provider list.
     let providers = await fetchOembedProviders();
