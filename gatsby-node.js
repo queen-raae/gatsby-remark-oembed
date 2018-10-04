659afd8e821e83d9c6349919a5c95820d9797340
Benedicte Raae <benedicte@lillylabs.no>
2018-10-04 13:59:35 +0200

    Memory problem solution #25 (#29)
    * Utilize gatsby cache for providers list.

---
+++ gatsby-node.js
@@ -0,0 +1,18 @@
+const {
+  ammendOptions,
+  ammendProviders,
+  filterProviders,
+  fetchOembedProviders
+} = require("./helpers");
+
+exports.onPreBootstrap = async ({ cache }, rawOptions) => {
+  const options = ammendOptions(rawOptions);
+  const rawProviders = await fetchOembedProviders();
+  const providers = processProviders(rawProviders, options);
+  await cache.set("oembed-providers", providers);
+};
+
+const processProviders = (providers, options) => {
+  providers = ammendProviders(providers);
+  return filterProviders(providers, options.providers);
+};
