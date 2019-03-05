1f2d51d1e57be0727eba2f7606c398176f6ebc5a
kennethormandy <hello@kennethormandy.com>
2019-03-04 22:22:28 -0800

    Adds use of formatted and raw options

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,11 +8,11 @@ const {
 exports.onPreBootstrap = async ({ cache }, rawOptions) => {
   const options = ammendOptions(rawOptions);
   const rawProviders = await fetchOembedProviders();
-  const providers = processProviders(rawProviders, options);
+  const providers = processProviders(rawProviders, options, rawOptions);
   await cache.set("remark-oembed-providers", providers);
 };
 
-const processProviders = (providers, options) => {
-  providers = ammendProviders(providers);
-  return filterProviders(providers, options.providers);
+const processProviders = (providers, optionsAsStrings, rawOptions) => {
+  providers = ammendProviders(providers, rawOptions);
+  return filterProviders(providers, optionsAsStrings.providers);
 };
