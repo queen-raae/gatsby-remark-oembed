428842d536988616f5551992ac2247ab0bb443cb
Benedicte Raae <raae@bgraphic.no>
2019-03-18 07:16:50 +0100

    Updated to npm package for gatsby-remark-oembed.

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
             }
           },
           {
-            resolve: `gatsby-remark-oembed`,
+            resolve: `@raae/gatsby-remark-oembed`,
             options: {
               providers: {
                 settings: {
