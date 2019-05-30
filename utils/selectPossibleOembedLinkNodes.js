cb2e69e9f62b00abe6c0b54c22a670cbc0870a4a
Phumrapee Limpianchop <git@rayriffy.com>
2019-05-30 19:48:23 +0700

    :bug: Fixes

--- utils/selectPossibleOembedLinkNodes.js
+++ utils/selectPossibleOembedLinkNodes.js
@@ -5,12 +5,13 @@ const selectPossibleOembedLinkNodes = (markdownAST, usePrefix = ['oembed:']) => 
   var res = [];
   nodes.map(node => {
     usePrefix.map(prefix => {
-      if (!node.value.startsWith(prefix)) return;
-    })
-    node.url = node.value.split(':');
-    node.url.shift();
-    node.url = node.url.join(':').trim();
-    res.push(node);
+      if (node.value.startsWith(prefix)) {
+        node.url = node.value.split(':');
+        node.url.shift();
+        node.url = node.url.join(':').trim();
+        return res.push(node);
+      }
+    });
   });
   return res;
 };
