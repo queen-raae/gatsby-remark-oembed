b3d02f476b8a31489488a62c3a7c70594f181f68
Phumrapee Limpianchop <git@rayriffy.com>
2019-05-30 19:24:19 +0700

    :sparkles: Handling with custom node prefix

--- utils/selectPossibleOembedLinkNodes.js
+++ utils/selectPossibleOembedLinkNodes.js
@@ -1,18 +1,18 @@
 const select = require("unist-util-select");
 
-const selectPossibleOembedLinkNodes = (markdownAST, usePrefix = false) => {
-  if (usePrefix === true) {
-    const nodes = select(markdownAST, "inlineCode");
-    var res = [];
-    nodes.map(node => {
-      if (!node.value.startsWith("oembed:")) return;
-      node.url = node.value.substring(7).trim();
-      res.push(node);
-    });
-    return res;
-  } else {
-    return select(markdownAST, "paragraph link:only-child");
-  }
+const selectPossibleOembedLinkNodes = (markdownAST, usePrefix = ['oembed:']) => {
+  const nodes = select(markdownAST, "inlineCode");
+  var res = [];
+  nodes.map(node => {
+    usePrefix.map(prefix => {
+      if (!node.value.startsWith(prefix)) return;
+    })
+    node.url = node.value.split(':');
+    node.url.shift();
+    node.url = node.url.join(':').trim();
+    res.push(node);
+  });
+  return res;
 };
 
 module.exports = selectPossibleOembedLinkNodes;
