a72f570b0c7dfc2b50b32789d4edc238babb0bda
Phumrapee Limpianchop <git@rayriffy.com>
2019-06-18 20:18:38 +0700

    :wheelchair: Backward compatibility

--- utils/selectPossibleOembedLinkNodes.js
+++ utils/selectPossibleOembedLinkNodes.js
@@ -2,19 +2,28 @@ const select = require("unist-util-select");
 
 const selectPossibleOembedLinkNodes = (
   markdownAST,
-  usePrefix = ["oembed:"]
+  usePrefix = false
 ) => {
   const nodes = select(markdownAST, "inlineCode");
   var res = [];
   nodes.map(node => {
-    usePrefix.map(prefix => {
-      if (node.value.startsWith(prefix)) {
-        node.url = node.value.split(":");
-        node.url.shift();
-        node.url = node.url.join(":").trim();
-        return res.push(node);
-      }
-    });
+    if (usePrefix === false) {
+      return select(markdownAST, "paragraph link:only-child");
+    } else if (usePrefix === true) {
+      if (!node.value.startsWith("oembed:")) return;
+      node.url = node.value.substring(7).trim();
+      res.push(node);
+      return res;
+    } else {
+      usePrefix.map(prefix => {
+        if (node.value.startsWith(prefix)) {
+          node.url = node.value.split(":");
+          node.url.shift();
+          node.url = node.url.join(":").trim();
+          return res.push(node);
+        }
+      });
+    }
   });
   return res;
 };
