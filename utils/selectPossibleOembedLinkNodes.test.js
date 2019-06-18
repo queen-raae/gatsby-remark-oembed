3c9ad44cde1d3790ecfeec0cff35a7d5e8ec6af7
Phumrapee Limpianchop <git@rayriffy.com>
2019-06-18 20:49:39 +0700

    :white_check_mark: Update test unit

--- utils/selectPossibleOembedLinkNodes.test.js
+++ utils/selectPossibleOembedLinkNodes.test.js
@@ -3,7 +3,7 @@ const MARKDOWN_AST = require("./.test/markdown");
 
 describe("#selectPossibleOembedLinkNodes", () => {
   test("select only links that are the only child of a paragraph", () => {
-    const possibleOembedLinks = selectPossibleOembedLinkNodes(MARKDOWN_AST);
+    const possibleOembedLinks = selectPossibleOembedLinkNodes(MARKDOWN_AST, true);
     expect(possibleOembedLinks).toHaveLength(1);
     expect(possibleOembedLinks[0]).toMatchObject({
       type: "inlineCode",
