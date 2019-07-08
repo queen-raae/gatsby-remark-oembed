3a634ce9b8cec641af03995030e181ddc77a0724
Benedicte Raae <raae@bgraphic.no>
2019-07-08 22:07:18 +0200

    Support true, false and array in usePrefix.

--- utils/amendOptions.js
+++ utils/amendOptions.js
@@ -1,10 +1,17 @@
 const { defaultsDeep } = require("lodash");
 
+const DEFAULT_USE_PREFIX = ["oembed:"];
+
 const DEFAULT_OPTIONS = {
-  usePrefix: ["oembed:"]
+  usePrefix: false
 };
 
 const amendOptions = options => {
+  if (options.usePrefix && !Array.isArray(options.usePrefix)) {
+    // usePrefix: true
+    options.usePrefix = DEFAULT_USE_PREFIX;
+  }
+
   return defaultsDeep({}, options, DEFAULT_OPTIONS);
 };
 
