70c77dbd33467c3419658ef0c6973f6fc2a4f273
Benedicte Raae <raae@raae.codes>
2019-12-30 15:31:15 +0100

    Swizec fix mdx support (#93)
    * Move provider fetching to its own file
    
    * Fetch providers when transforming nodes, if not available
    
    Co-authored-by: Swizec Teller <swizec@swizec.com>

--- gatsby-remark-oembed/utils/index.js
+++ gatsby-remark-oembed/utils/index.js
@@ -8,6 +8,7 @@ const getProviderEndpointForLinkUrl = require("./getProviderEndpointForLinkUrl")
 const selectPossibleOembedLinkNodes = require("./selectPossibleOembedLinkNodes");
 const tranformsLinkNodeToOembedNode = require("./tranformsLinkNodeToOembedNode");
 const logResults = require("./logResults");
+const getProviders = require('./getProviders')
 
 exports.amendOptions = amendOptions;
 exports.fetchOembedProviders = fetchOembedProviders;
@@ -19,3 +20,4 @@ exports.fetchOembed = fetchOembed;
 exports.selectPossibleOembedLinkNodes = selectPossibleOembedLinkNodes;
 exports.tranformsLinkNodeToOembedNode = tranformsLinkNodeToOembedNode;
 exports.logResults = logResults;
+exports.getProviders = getProviders;
