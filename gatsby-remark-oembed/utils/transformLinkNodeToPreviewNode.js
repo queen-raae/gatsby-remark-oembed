c1149e6c26dcdb6548c4a1be13b56fe7e3a0e5d3
Anindha Parthy <anindha@gmail.com>
2020-05-13 16:39:03 +1000

    Use url from node if OG url is missing

--- gatsby-remark-oembed/utils/transformLinkNodeToPreviewNode.js
+++ gatsby-remark-oembed/utils/transformLinkNodeToPreviewNode.js
@@ -1,20 +1,25 @@
 var url = require("url");
 
 const transformLinkNodeToPreviewNode = (node, metaData) => {
-  const host = url.parse(metaData.open_graph.url).host;
+  const linkUrl = metaData.open_graph.url || node.url;
+  const host = url.parse(linkUrl).host;
 
   node.type = "html";
   node.value = `
       <div class="gatsby-remark-oembed-preview-wrapper">
-        <a href="${metaData.open_graph.url}" class="gatsby-remark-oembed-preview-link">
+        <a href="${linkUrl}" class="gatsby-remark-oembed-preview-link">
           <img src="${metaData.open_graph.images[0].url}"
             class="gatsby-remark-oembed-preview-image"
             width="100%"
             title="${metaData.open_graph.title}"
           />
           <p class="gatsby-remark-oembed-preview-host">${host}</p>
-          <h3 class="gatsby-remark-oembed-preview-title">${metaData.open_graph.title}</h3>
-          <p class="gatsby-remark-oembed-preview-description">${metaData.open_graph.description}</p>
+          <h3 class="gatsby-remark-oembed-preview-title">${
+            metaData.open_graph.title || ""
+          }</h3>
+          <p class="gatsby-remark-oembed-preview-description">${
+            metaData.open_graph.description || ""
+          }</p>
         </a>
       </div>
     `;
