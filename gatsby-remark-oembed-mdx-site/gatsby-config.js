9c6b95053e199d6fb711fbb041774074b8b76046
Benedicte Raae <raae@bgraphic.no>
2020-10-30 07:43:00 +0100

    Update mdx site metadata/content

--- gatsby-remark-oembed-mdx-site/gatsby-config.js
+++ gatsby-remark-oembed-mdx-site/gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: "gatsby-remark-oembed plugin example site",
+    title: "gatsby-remark-oembed plugin MDX example site",
     author: "raae.codes",
-    description: "A site demonstrating the gatsby-remark-oembed plugin",
-    siteUrl: "https://gatsby-remark-oembed.netlify.com/",
+    description:
+      "A site demonstrating the gatsby-remark-oembed plugin used with gatsby-plugin-mdx",
+    siteUrl: "https://gatsby-remark-oembed-mdx.netlify.com/"
   },
   pathPrefix: "/",
   plugins: [
@@ -11,8 +12,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/posts`,
-        name: "posts",
-      },
+        name: "posts"
+      }
     },
     {
       resolve: `gatsby-plugin-mdx`,
@@ -26,37 +27,37 @@ module.exports = {
                 exclude: ["Twitter"],
                 settings: {
                   Twitter: {
-                    theme: "light", // Use the Twitter dark theme
+                    theme: "light" // Use the Twitter dark theme
                   },
                   Instagram: {
-                    hidecaption: true,
-                  },
-                },
-              },
-            },
+                    hidecaption: true
+                  }
+                }
+              }
+            }
           },
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
-            },
+              maxWidth: 590
+            }
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-        ],
-      },
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
+          }
+        ]
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: "src/utils/typography",
-      },
-    },
-  ],
+        pathToConfigModule: "src/utils/typography"
+      }
+    }
+  ]
 };
