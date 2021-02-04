c6f03d46ddfe4c0f6887e8e9b6b896507b3e61ca
Benedicte Raae <raae@bgraphic.no>
2021-02-04 21:36:54 +0100

    Clean up infrastructure

--- gatsby-remark-oembed-mdx-site/gatsby-config.js
+++ gatsby-remark-oembed-mdx-site/gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     author: "raae.codes",
     description:
       "A site demonstrating the gatsby-remark-oembed plugin used with gatsby-plugin-mdx",
-    siteUrl: "https://gatsby-remark-oembed-mdx.netlify.com/"
+    siteUrl: "https://gatsby-remark-oembed-mdx.netlify.com/",
   },
   pathPrefix: "/",
   plugins: [
@@ -14,8 +14,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/../example-content/posts`,
-        name: "posts"
-      }
+        name: "posts",
+      },
     },
     {
       resolve: `gatsby-plugin-mdx`,
@@ -28,38 +28,39 @@ module.exports = {
               providers: {
                 settings: {
                   Twitter: {
-                    theme: "light" // Use the Twitter light theme
+                    theme: "light", // Use the Twitter light theme
                   },
                   Instagram: {
                     hidecaption: false,
-                    access_token: process.env.INSTAGRAM_ACCESS_TOKEN
-                  }
-                }
-              }
-            }
+                    access_token: process.env.INSTAGRAM_ACCESS_TOKEN,
+                  },
+                  test: "testing",
+                },
+              },
+            },
           },
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590
-            }
+              maxWidth: 590,
+            },
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`
-            }
-          }
-        ]
-      }
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+        ],
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: "src/utils/typography"
-      }
-    }
-  ]
+        pathToConfigModule: "src/utils/typography",
+      },
+    },
+  ],
 };
